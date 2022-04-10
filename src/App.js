81aff33b069abba88aecf0bdf593fade669d7bc0
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-04-10 21:41:48 +0500

    send message functionality done

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import {GoogleAuthProvider, getAuth, signOut, signInWithPopup} from 'firebase/au
 
 import {useAuthState} from "react-firebase-hooks/auth";
 import {useCollectionData} from "react-firebase-hooks/firestore";
-import React from "react";
+import React, {useState} from "react";
 import env from "react-dotenv";
-import {collection, query, limit, orderBy, getDocs} from "firebase/firestore";
+import {collection, query, limit, orderBy, addDoc, serverTimestamp} from "firebase/firestore";
 
 const config = {
     apiKey: env.apiKey,
@@ -57,26 +57,43 @@ function SignOut() {
 function ChatRoom() {
     const messagesRef = collection(firestore, 'messages');
     const q = query(messagesRef, orderBy("createdAt"), limit(25));
-
     const [messages] = useCollectionData(q, {idField: 'id'});
-    console.log(messages)
+
+    const [formData, setFormData] = useState('');
+
+    const sendMessage = async(e) => {
+        e.preventDefault();
+        const {uid, photoURL } = auth.currentUser;
+
+        await addDoc(messagesRef, {
+            text: formData,
+            createdAt: serverTimestamp(),
+            uid,
+            photoURL
+        })
+        setFormData('');
+    }
+
     return (
         <>
             <div>
                 {messages && messages.map((message, index) => (<ChatMessage key={index} message={message}/>))}
             </div>
 
-            form
+            <form onSubmit={sendMessage}>
+                <input value={formData} onChange={(e) => setFormData(e.target.value)}/>
+                <button type={'submit'}>Send</button>
+            </form>
         </>
     )
 }
 
 function ChatMessage(props) {
-    const {text, uid, photoUrl} = props.message;
+    const {text, uid, photoURL} = props.message;
     const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
     return (
         <div className={`message ${messageClass}`}>
-            <img src={photoUrl}/>
+            <img src={photoURL}/>
             <p>{text}</p>
         </div>
     )
